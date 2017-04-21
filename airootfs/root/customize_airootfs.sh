66bfef28778128b034d6d1ad038018e0f0471f44
Neil Cecchini <stranger.neil@gmail.com>
2017-04-21 15:00:05 +0200

    Large edits were made and will probably be reset, first attempt at an image

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -5,19 +5,28 @@ set -e -u
 sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
-ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 
+# Create and configure users
 usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
 chmod 700 /root
 
-sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
+useradd -mU -s /usr/bin/zsh student
+useradd -mUG wheel -s /usr/bin/zsh cobra
+chpasswd <<< "cobra:cobra42"
+
+# Allow the wheel group to use sudo
+sed -i 's/# \(%wheel ALL=(ALL) NOPASSWD: ALL\)/\1' /etc/sudoers
+
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 
+# Disable power keys
+sed -i 's/#\(HandlePowerKey=\)poweroff/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
-systemctl enable pacman-init.service choose-mirror.service
-systemctl set-default multi-user.target
+systemctl enable pacman-init.service choose-mirror.service nodm.service
+systemctl set-default graphical.target

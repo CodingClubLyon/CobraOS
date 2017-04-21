7b4e76266a27830671296dc5c19cd62dd138a4a2
Neil Cecchini <stranger.neil@gmail.com>
2017-04-21 17:13:25 +0200

    Second iteration

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -17,16 +17,15 @@ useradd -mUG wheel -s /usr/bin/zsh cobra
 chpasswd <<< "cobra:cobra42"
 
 # Allow the wheel group to use sudo
-sed -i 's/# \(%wheel ALL=(ALL) NOPASSWD: ALL\)/\1' /etc/sudoers
+sed -i 's/# \(%wheel ALL=(ALL) NOPASSWD: ALL\)/\1/' /etc/sudoers
 
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 
 # Disable power keys
-sed -i 's/#\(HandlePowerKey=\)poweroff/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 
-systemctl enable pacman-init.service choose-mirror.service nodm.service
-systemctl set-default graphical.target
+systemctl enable pacman-init.service choose-mirror.service nodm.service hostname.service NetworkManager.service
+systemctl set-default multi-user.target
